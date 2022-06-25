44d8704de69175d4c7596608bf9198a141b007c0
Damian Dzialo <ddzialo28@interia.pl>
2022-06-25 22:27:37 +0200

    Change server cors options

--- keystone.ts
+++ keystone.ts
@@ -35,7 +35,10 @@ const whiteList = [ 'http://localhost:7777', 'https://sharred.vercel.app/' ];
 export default withAuth(
   config({
     server: {
-      cors: { origin: whiteList },
+      cors: { origin: whiteList, 
+        preflightContinue: false,
+        optionsSuccessStatus: 204,
+      },
       port: parseInt(process.env.PORT!) || 3000,
       maxFileSize: 200 * 1024 * 1024,
       healthCheck: true,
