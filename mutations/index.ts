025b2a3abd5dc87da5fbf962b814034188a02280
Damian Dzialo <ddzialo28@interia.pl>
2022-08-23 18:54:40 +0200

    Add addProduct custom mutation test

--- mutations/index.ts
+++ mutations/index.ts
@@ -1,15 +1,56 @@
+/* eslint-disable @typescript-eslint/restrict-plus-operands */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { graphql, graphQLSchemaExtension } from "@keystone-6/core";
-import addProductToBag from "./addProductToBag";
+import { Context } from ".keystone/types";
 
-export const extendGraphqlSchema = graphQLSchemaExtension({
+export const extendGraphqlSchema = graphQLSchemaExtension<Context>({
   typeDefs: `
-        type Mutation {
-            addProductToBag(productId: ID): Cart
-        }
+       type Mutation {
+      checkCart(id: ID!): Cart
+      addProductToBag(productId: ID!): [Cart]
+    }
     `,
   resolvers: {
     Mutation: {
-      addProductToBag,
+      checkCart: (root, { id }, context) => {
+        const cart = context.query.Cart.findOne({
+          where: { id },
+          query: "id user { id } product { id } quantity",
+        });
+
+        return cart;
+      },
+      addProductToBag: async (root, { productId }, context) => {
+        // check session
+        if (!context.session.itemId) {
+          throw new Error("Not logged in");
+        }
+
+        const cartItem = await context.query.Cart.findMany({
+          where: {
+            user: { id: context.session.itemId },
+            product: { id: productId },
+          },
+          query: "id quantity",
+        });
+
+        const [cartCheck] = cartItem;
+        if (cartCheck) {
+          return await context.query.Cart.updateOne({
+            where: { id: cartCheck.id },
+            data: { quantity: cartCheck.quantity + 1 },
+          });
+        }
+
+        return await context.query.Cart.createOne({
+          data: {
+            user: { connect: { id: context.session.itemId } },
+            product: { connect: { id: productId } },
+            quantity: 1,
+          },
+        });
+      },
     },
   },
 });
